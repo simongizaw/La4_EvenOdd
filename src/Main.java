27cd3b5bc1c7a4a3cd321e5fa66a3abcf7f79fda
simon <simongizaw@gmail.com>
2022-10-05 12:03:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 9;
+
+
+        if((num % 2)==0){
+
+            System.out.println("Your number is even");
+
+
+        } else {
+            System.out.println("your number is odd");
+        }
+
+    }
+}
